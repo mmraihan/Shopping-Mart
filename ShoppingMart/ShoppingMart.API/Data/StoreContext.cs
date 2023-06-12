18f183011b4bc85dc9fcd97b866a021936d2bddd
mmraihan <mm_raihan@live.com>
2023-06-12 14:00:54 +0600

    Setting up the connection string. -APIBasics

---
+++ ShoppingMart/ShoppingMart.API/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingMart.API.Entities;
+
+namespace ShoppingMart.API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ShoppingMart/ShoppingMart.API/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingMart.API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
