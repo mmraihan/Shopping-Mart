12d3cdc853e0dc5af7e4fde7f5cc0c3af1dd109b
mmraihan <mm_raihan@live.com>
2023-06-22 13:54:54 +0600

    Setting up Generic Repository.  -GenericRepo

---
+++ ShoppingMart/ShoppingMart.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingMart.Core.Entities;
+using ShoppingMart.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingMart.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
