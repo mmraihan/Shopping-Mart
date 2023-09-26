4ce7fb117f956e2fcb421f7de8cf1dd01970baf7
mmraihan <mm_raihan@live.com>
2023-09-27 00:24:10 +0600

    Search: Adding the search functionality.

--- ShoppingMart/ShoppingMart.Core/Specifications/ProductsWithFiltersForCountSpecification.cs
+++ ShoppingMart/ShoppingMart.Core/Specifications/ProductsWithFiltersForCountSpecification.cs
@@ -10,6 +10,7 @@ namespace ShoppingMart.Core.Specifications
     public class ProductsWithFiltersForCountSpecification : BaseSpecification<Product>
     {
         public ProductsWithFiltersForCountSpecification(ProductSpecParams productParams) : base(x =>
+             (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
              (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
              (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
         {
--- ShoppingMart/ShoppingMart.Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ ShoppingMart/ShoppingMart.Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -12,6 +12,7 @@ namespace ShoppingMart.Core.Specifications
     {      
             public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
                 :base(x=>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
                 (!productParams.BrandId.HasValue ||x.ProductBrandId== productParams.BrandId) &&
                 (!productParams.TypeId.HasValue || x.ProductTypeId== productParams.TypeId))
             {
