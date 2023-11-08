8f994fd8b9c8a6012e9681b8ef9a739da0121ba1
mmraihan <mm_raihan@live.com>
2023-11-08 12:32:09 +0600

    13.Basket: Setting up Basket Class.

---
+++ ShoppingMart/ShoppingMart.Core/Entities/CustomerBasket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingMart.Core.Entities
+{
+    public class CustomerBasket
+    {
+        public CustomerBasket(int id )
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+        public List<BasketItem> items { get; set; } = new List<BasketItem>();
+
+    }
+}
