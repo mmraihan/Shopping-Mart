72df17deda35c371b1e71c59aee838e2b4751dae
mmraihan <mm_raihan@live.com>
2023-11-08 14:38:18 +0600

    13.Basket: Basket Controller Implemented.

---
+++ ShoppingMart/ShoppingMart.API/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingMart.Core.Entities;
+using ShoppingMart.Core.Interfaces;
+
+namespace ShoppingMart.API.Controllers
+{
+  
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketId(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+            return Ok(basket?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updatedData = await _basketRepository.UpdateBasketAsync(basket);
+            return Ok(updatedData);
+        }
+
+        [HttpDelete]
+
+        public async Task DeleteBasket(string id)
+        {
+            await _basketRepository.DeleteBasketAsync(id);
+        }
+    }
+}
