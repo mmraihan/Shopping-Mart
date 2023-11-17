5ea51c965b45851b7e2fc6b675489beb89678caf
mmraihan <mm_raihan@live.com>
2023-11-17 15:26:47 +0600

    14.Basket: Environment for base url setup both in development and production.

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+
+  basUl= environment.apiUrl
 
   constructor() { }
 }
