722734c1611683b811f807da82ac3ade4707175e
mmraihan <mm_raihan@live.com>
2023-11-17 15:45:38 +0600

    14.Basket: Implemented Basket necessary services.

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,12 +1,36 @@
+import { Basket } from './../shared/models/basket';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
 
-  basUl= environment.apiUrl
+  baseUl= environment.apiUrl
+  private basketSource= new BehaviorSubject<Basket | null>(null);
+  basketSource$ = this.basketSource.asObservable();
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  getBasket(id: string){
+    return this.http.get<Basket>(this.baseUl+'basket?id'+ id).subscribe({
+      next: basket=>this.basketSource.next(basket)
+    })
+  }
+
+  setBasket(basket: Basket){
+    return this.http.post<Basket>(this.baseUl+ 'basket', basket).subscribe({
+      next: basket=> this.basketSource.next(basket);
+    })
+  }
+
+  getCurrentBasketValue(){
+    return this.basketSource.value;
+  }
+
+
 }
