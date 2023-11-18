aa7526e46e96f858e6dd71cc1b11e4c9cc35063a
mmraihan <mm_raihan@live.com>
2023-11-18 22:58:32 +0600

    11.Client-ErrorHandling: Created Error component for testing errors.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
+  {path: 'test-error', component: TestErrorComponent},
   {
     path: 'shop',
     loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,13 +5,15 @@ import { RouterModule } from '@angular/router';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { TestErrorComponent } from './test-error/test-error.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    SectionHeaderComponent
+    SectionHeaderComponent,
+    TestErrorComponent
   ],
   imports: [
     CommonModule,
