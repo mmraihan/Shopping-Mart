10199159e167468716e999dba06eca180f648904
mmraihan <mm_raihan@live.com>
2023-11-19 10:51:39 +0600

    11: Implement Interceptor for catching error from api and configure it.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,11 @@ import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
     HomeModule
 
   ],
-  providers: [ {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},],
+  
+  providers: [ 
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
