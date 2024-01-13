1f1dcb5c17d2f9800f6e66a0f0a994836ff7b1bc
mmraihan <mm_raihan@live.com>
2024-01-14 01:06:38 +0600

    17.Identity Client: Implement account services.

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../shared/models/User';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: Router) {}
+
+  login(values: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', values).pipe(
+      map((user) => {
+        localStorage.setItem('token', user.token);
+        this.currentUserSource.next(user);
+      })
+    );
+  }
+
+  register(values: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', values).pipe(
+      map((user) => {
+        localStorage.setItem('token', user.token);
+        this.currentUserSource.next(user);
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.route.navigateByUrl('/');
+  }
+
+  checkEmailExist(email: string) {
+    return this.http.get<boolean>(
+      this.baseUrl + 'account/emailExists?email=' + email
+    );
+  }
 }
