282a437f78070e780d118da43533ce7b73cf4a59
mmraihan <mm_raihan@live.com>
2024-01-20 00:20:36 +0600

    17.Identity Client: Async Validator in Email form control.

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,39 +1,68 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, ElementRef } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
+import { debounceTime, finalize, map, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+  ) {}
 
-  constructor(private fb: FormBuilder, private accountService: AccountService,
-    private router: Router) { }
+  complexPassword =
+    "(?=^.{6,10}$)(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*s).*$";
 
-  complexPassword = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$"
-
-   errors: string[] | null= null;
+  errors: string[] | null = null;
 
   //testing = Abcd@5678
 
   registerForm = this.fb.group({
     displayName: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.pattern(this.complexPassword)]],
-  })
-
+    email: [
+      '',
+      [Validators.required, Validators.email],
+      [this.validateEmailNotTaken()],
+    ],
+    password: [
+      '',
+      [Validators.required, Validators.pattern(this.complexPassword)],
+    ],
+  });
 
   //-----------------All Methods--------------------
 
   onSubmit() {
     this.accountService.register(this.registerForm.value).subscribe({
       next: () => this.router.navigateByUrl('/shop'),
-      error:(err)=> this.errors=err.errors,  
-    })
+      error: (err) => (this.errors = err.errors),
+    });
   }
 
-
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        debounceTime(1000),
+        take(1),
+        switchMap(() => {
+          return this.accountService.checkEmailExist(control.value).pipe(
+            map((result) => (result ? { emailExists: true } : null)),
+            finalize(() => control.markAsTouched())
+          );
+        })
+      );
+    };
+  }
+  
 }
